9fd36008c75927a74e89bd3357cc1ac68f6bcdc2
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-09 19:13:59 +0100

    Added more track

--- roll the ball/Assets/Script/CharacterController.cs
+++ roll the ball/Assets/Script/CharacterController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public Vector3 force;
+    public Vector3 forceRight;
+    public Vector3 forceForward;
 
     private void FixedUpdate() {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<Rigidbody>().velocity += force;
+        if (Input.GetKey(KeyCode.RightArrow)){
+            GetComponent<Rigidbody>().velocity += forceRight;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody>().velocity -= force;
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            GetComponent<Rigidbody>().velocity -= forceRight;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            GetComponent<Rigidbody>().velocity += forceForward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            GetComponent <Rigidbody>().velocity -= forceForward;
         }
     }
 }
